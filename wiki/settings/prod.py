25beb3ca97a130274c65de32991ce5699f6757c7
chiqui-m <chiquimarquez@gmail.com>
2022-01-24 18:02:07 -0600

    removed references to AWS

--- wiki/settings/prod.py
+++ wiki/settings/prod.py
@@ -12,39 +12,43 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1', cast=Csv())
 
 
 #AWS S3 settings
-AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+#AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 
-AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+#AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 
-AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+#AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
 
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+#AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 #ex: http://my-app-bucket.s3.amazonaws.com/static/
 
-AWS_DEFAULT_ACL = 'public-read'
+#AWS_DEFAULT_ACL = 'public-read'
 
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400'
-}
+#AWS_S3_OBJECT_PARAMETERS = {
+#    'CacheControl': 'max-age=86400'
+#}
 
-AWS_LOCATION = 'static'
+#AWS_LOCATION = 'static'
 
-AWS_QUERYSTRING_AUTH = False
+#AWS_QUERYSTRING_AUTH = False
 
-AWS_HEADERS = {
-    'Access-Control-Allow-Origin': '*',
-}
+#AWS_HEADERS = {
+#    'Access-Control-Allow-Origin': '*',
+#}
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+#STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+#MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+
+STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
 
 
 #Heroku logging
